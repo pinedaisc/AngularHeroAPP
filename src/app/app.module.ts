4e718203d6757b0d6857e628008630fb801def3f
pinedaisc <pinedaisc@gmail.com>
2018-09-12 17:36:57 -0500

    fist committte

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+
+import { APP_ROUTING } from './app.routes';
+
+import { HeroesService } from './services/heroes.service';
+import { HeroebioComponent } from './components/heroebio/heroebio.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroebioComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [
+    HeroesService
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/app/components/heroebio/heroebio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroebio',
+  templateUrl: './heroebio.component.html',
+  styles: []
+})
+export class HeroebioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
